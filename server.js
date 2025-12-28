309c97de063ac230ab61ceb5fce9e86cae35812f
alizulfiqarbuksh <ali.zulfiqarbuksh@gmail.com>
2025-12-28 10:35:24 +0300

    git innit

---
+++ server.js
@@ -0,0 +1,20 @@
+const dotenv =  require('dotenv');
+dotenv.config();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('connected', () => {
+  console.log("Connected to DB")
+})
+
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+app.listen(3000, () => {
+  console.log('express is ready')
+});
